28c67b9e44936e86399985c5b3b33de9793a5c50
reevr <reev.r00@gmail.com>
2018-02-14 18:00:52 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('working'); 
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`server started on port : ${port}`);
+});
